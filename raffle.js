d15844219893568503b0cea1b53b7628fa698dc6
ground.contact <ground.contact@gmail.com>
2016-12-13 19:50:30 +0000

    update raffle to fit two columns only

--- raffle.js
+++ raffle.js
@@ -1,34 +1,27 @@
-$(document).ajaxSuccess(function(evnt, xhr, settings) {
-	if(settings.url == "https://raw.githubusercontent.com/ENEUE/eneue.github.io/gh/raffle.js"){
-        window.raffleParticipants = JSON.parse(xhr.responseText);
-        $.each(window.raffleParticipants, function(key, data) {
-        	var tableRaffle = $('#raffleParticipantsTable');
-        	var tableRow = $('<tr/>')
-             	.addClass('ui-tr')
-              	.appendTo(tableRaffle);
-            var tableItem = $('<td/>')
-            	.addClass('ui-td-mail')
-            	.text(key)
-            	.appendTo(tableRow);
-        	$.each(data, function(index, datae) {
-        		if(datae == "true") {
-        			datae = "Sí";
-        		} else if (datae == "false") {
-        			datae = "No";
-        		}
-        		tableItem = $('<td/>')
-            		.addClass('ui-td')
-            		.text(datae)
-            		.appendTo(tableRow);
-        	});
-        });
-     	$("#raffleParticipantsTable").tablesorter({sortList: [[1,0]]});
-    }
-});
-  
 var url_raffle = "https://raw.githubusercontent.com/ENEUE/eneue.github.io/gh/raffle.js"
 $.ajax({
   type: 'GET',
   dataType: 'text', //use jsonp data type in order to perform cross domain ajax
   crossDomain: true,
   url: url_raffle}) ;
+
+$(document).ajaxSuccess(function(evnt, xhr, settings) {
+  if(settings.url == "https://raw.githubusercontent.com/ENEUE/eneue.github.io/gh/raffle.js"){
+        window.raffleParticipants = JSON.parse(xhr.responseText);
+        $.each(window.raffleParticipants, function(key, data) {
+          console.log(key + "   " + data);
+          var tableRaffle = $('#raffleParticipantsTable');
+          var tableRow = $('<tr/>')
+              .addClass('ui-tr')
+                .appendTo(tableRaffle);
+            var tableItem = $('<td/>')
+              .addClass('ui-td-mail')
+              .text(key)
+              .appendTo(tableRow);
+            $('<td/>')
+              .text(data)
+              .appendTo(tableRow);
+        });
+      $("#raffleParticipantsTable").tablesorter({sortList: [[1,0]]});
+    }
+});
