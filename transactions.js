ef3db2804aac3a909bb22dc12e8083089b1dda31
ground.contact <ground.contact@gmail.com>
2016-12-14 16:31:58 +0000

    Live key

--- transactions.js
+++ transactions.js
@@ -493,7 +493,7 @@ function closeWaitDiv(id) {
 
 //Declares the Stripe Checkout Handler and configures it
 var handler = StripeCheckout.configure({
-    key: 'pk_test_AfqpiD3DBLtXD8u39JwGErf8',
+    key: 'pk_live_uWo17rHYl0BXyyKmoMtHM3aS',
     //***************************************************************IMPORTANT!!!!!!!------CHECK IMAGE STORAGE AND PARAMETERS FOR CHECKOUT**********
     image: 'https://estonoesunaescuela.squarespace.com/s/anagrama_peq_color_whitebckgrnd_small.png',
     locale: 'auto',
